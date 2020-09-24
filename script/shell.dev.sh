a59d43958f092678b5143214d00c72ac44a08de2
zengqi <136800645@qq.com>
2020-09-24 10:48:13 +0800

    tst

--- script/shell.dev.sh
+++ script/shell.dev.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 node -v
-#检查编译环境
+# 检查编译环境
 npm -v 
 npm install chromedriver --chromedriver_cdnurl=http://cdn.npm.taobao.org/dist/chromedriver
 npm install 
-#编译项目
+# 编译项目
 npm run build 
 cd build
-#把所有文件压缩
+# 把所有文件压缩
 tar -zcvf dist.tar.gz * 
-#压缩文件解压到nginx映射目录
+# 压缩文件解压到nginx映射目录
 tar -zxvf /root/.jenkins/workspace/react_demo/build/dist.tar.gz -C /usr/share/nginx/html 
 cd /root/.jenkins/workspace/react_demo
-#删除项目打包后的残留
+# 删除项目打包后的残留
 rm -R build 
