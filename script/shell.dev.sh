c3bedff32862c79b563d1a1e895728ee551820dc
zengqi <136800645@qq.com>
2020-09-24 11:38:44 +0800

    s

--- script/shell.dev.sh
+++ script/shell.dev.sh
@@ -6,6 +6,7 @@ npm install
 var_path = `pwd`
 echo $var_path
 npm run build
+cd ..
 cd build
 tar -zcvf dist.tar.gz *
 tar -zxvf /root/.jenkins/workspace/react_demo/build/dist.tar.gz -C /usr/share/nginx/html
