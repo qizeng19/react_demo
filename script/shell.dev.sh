a71ec7c4196407546c4132cdff796b425eb3b28f
zengqi <136800645@qq.com>
2020-09-24 11:03:03 +0800

    tst

--- script/shell.dev.sh
+++ script/shell.dev.sh
@@ -1,3 +1,9 @@
 node -v
 npm -v
 npm install
+npm run build
+cd build
+tar -zcvf dist.tar.gz *
+tar -zxvf /root/.jenkins/workspace/react_demo/build/dist.tar.gz -C /usr/share/nginx/html
+cd /root/.jenkins/workspace/react_demo
+rm -R build
